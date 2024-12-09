c2d92c2b2436c5f893a19f660434c0c0f5947925
= <a965225878@126.com>
2024-12-09 08:30:05 +0800

    Example 09 Texture and Sprite v0.5

--- main.cpp
+++ main.cpp
@@ -10,35 +10,18 @@ int main()
 {
     srand(time(0));
 
-    RenderWindow window(sf::VideoMode(640, 480), "Ball Shooter", sf::Style::Default);
+    RenderWindow window(sf::VideoMode(640, 480), "Example 09", sf::Style::Default);
     window.setFramerateLimit(60);
 
-    CircleShape bullet;
-    std::vector<CircleShape> vec_bullets;
-    RectangleShape enemy;
-    std::vector<RectangleShape> enemies;
-    CircleShape palyer;
+    Texture smileTex;
+    Sprite smile;
 
-    int shootTimer = 0;
-    int bulletCold = 8;
-    int enemySpawnTimer = 0;
-    int enemyCold = 20;
-    float bulletVcc = 5;
-    float enemyVcc = 1;
-    ulong enemyCount = 8;
-
-    enemy.setFillColor(Color::Red);
-    enemy.setSize(Vector2f(40, 30));
-    enemy.setOrigin(enemy.getSize().x / 2.f, enemy.getSize().y / 2);
-
-    bullet.setRadius(10);
-    bullet.setFillColor(Color::Blue);
-    bullet.setOrigin(bullet.getRadius(), bullet.getRadius());
-
-    palyer.setFillColor(Color::Green);
-    palyer.setRadius(20);
-    palyer.setOrigin(palyer.getRadius(), palyer.getRadius());
-    palyer.setPosition(640 / 2.f, 450);
+    if (!smileTex.loadFromFile("../../Pictures/smile.png"))
+    {
+        throw "can not load smile.png";
+    }
+    smile.setTexture(smileTex);
+    smile.setScale(0.5, 0.5);
 
     while (window.isOpen())
     {
@@ -51,80 +34,9 @@ int main()
                 window.close();
         }
 
-        // Update
-        // palyer
-        palyer.setPosition(Mouse::getPosition(window).x, palyer.getPosition().y);
-
-        if (Mouse::isButtonPressed(Mouse::Left) && shootTimer >= bulletCold)
-        {
-            bullet.setPosition(palyer.getPosition());
-            vec_bullets.push_back(CircleShape(bullet));
-
-            shootTimer = 0;
-        }
-
-        // bullet
-        if (shootTimer < bulletCold)
-            shootTimer++;
-        for (size_t i = 0; i < vec_bullets.size(); i++)
-        {
-            vec_bullets[i].move(0, -bulletVcc);
-
-            if (vec_bullets[i].getPosition().y < -bullet.getRadius())
-            {
-                vec_bullets.erase(vec_bullets.begin() + i);
-            }
-        }
-
-        // enemy
-        if (enemySpawnTimer < enemyCold)
-            enemySpawnTimer++;
-
-        if (enemySpawnTimer >= enemyCold && enemies.size() < enemyCount)
-        {
-            enemySpawnTimer = 0;
-            enemy.setPosition(rand() % (int)(window.getSize().x - enemy.getSize().x), -enemy.getSize().y);
-            enemies.push_back(RectangleShape(enemy));
-        }
-        for (size_t i = 0; i < enemies.size(); i++)
-        {
-            enemies[i].move(0, enemyVcc);
-
-            if (enemies[i].getPosition().y > palyer.getPosition().y + 10)
-            {
-                enemies.erase(enemies.begin() + i);
-            }
-        }
-
-        // collision
-        for (size_t i = 0; i < enemies.size(); i++)
-        {
-            for (size_t j = 0; j < vec_bullets.size(); j++)
-            {
-                if (vec_bullets[j].getGlobalBounds().intersects(enemies[i].getGlobalBounds()))
-                {
-                    vec_bullets.erase(vec_bullets.begin() + j);
-                    enemies.erase(enemies.begin() + i);
-
-                    break;
-                }
-            }
-        }
-
-        // Draw
         window.clear(Color(125, 200, 125, 200));
 
-        for (size_t i = 0; i < enemies.size(); i++)
-        {
-            window.draw(enemies[i]);
-        }
-
-        for (size_t i = 0; i < vec_bullets.size(); i++)
-        {
-            window.draw(vec_bullets[i]);
-        }
-
-        window.draw(palyer);
+        window.draw(smile);
 
         window.display();
     }
